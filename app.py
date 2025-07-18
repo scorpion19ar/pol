e238f67740111f84a01c34d56e96042e065ad974
scorpion19ar <111805434+scorpion19ar@users.noreply.github.com>
2025-07-18 20:12:18 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,16 @@
-
 from flask import Flask, jsonify
+from data_service import obtener_partidos
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def home():
     return '¡Hola desde Flask en Render!'
 
-@app.route('/pronosticos')
-def pronosticos():
-    return jsonify([
-        {"partido": "Godoy Cruz vs Sarmiento", "gana": "Godoy Cruz"},
-        {"partido": "River Plate vs Instituto", "gana": "River Plate"},
-        {"partido": "Boca Juniors vs Unión", "gana": "Boca Juniors"},
-        {"partido": "Estudiantes vs Huracán", "gana": "Estudiantes"},
-        {"partido": "Atlético Tucumán vs Central Córdoba", "gana": "Atlético Tucumán"},
-        {"partido": "Newell's vs Banfield", "gana": "Newell's"},
-        {"partido": "San Lorenzo vs Gimnasia", "gana": "San Lorenzo"},
-        {"partido": "Lanús vs Rosario Central", "gana": "Lanús"},
-        {"partido": "Vélez vs Platense", "gana": "Vélez"},
-        {"partido": "Racing vs Belgrano", "gana": "Racing Club"}
-    ])
+@app.route('/datos')
+def datos():
+    datos_partidos = obtener_partidos()
+    return jsonify(datos_partidos)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+    app.run(debug=True)
